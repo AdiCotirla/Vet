dd07083a9a7cb89c3a6d091c896648b6bec80dc3
AdiCotirla <adioprea53@gmail.com>
2022-03-09 19:18:48 +0200

    sa

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
   <Router>
     <Routes>
-      <Route exact path='/' element={<> <Clinic/> <Navbar/></>}/>
+      <Route exact path='/Vet/' element={<> <Clinic/> <Navbar/></>}/>
   </Routes>
   </Router>
   );
