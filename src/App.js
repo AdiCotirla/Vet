acca8bdd518955c38091aa55659bc5db33545b41
AdiCotirla <adioprea53@gmail.com>
2022-03-09 19:25:43 +0200

    sad

--- src/App.js
+++ src/App.js
@@ -10,11 +10,13 @@ import Clinic from './Components/Clinic/Clinic';
 function App() {
   return (
   <Router basename='/Vet'>
+    <div className='App'>
     <Clinic/>
      <Navbar/>
     <Routes>
       <Route exact path='/' element={<> <Clinic/> <Navbar/></>}/>
   </Routes>
+  </div>
   </Router>
   );
 }
